79b4d92f2980500a12f969c4a41713cd849f5d5a
shanti <abc@demo.in>
2018-12-18 12:57:06 +0530

    "Commit"

--- DemoOfGitForMultiDevelopers/src/Demo2.java
+++ DemoOfGitForMultiDevelopers/src/Demo2.java
@@ -3,7 +3,7 @@ public class Demo2 {
 	
 	public static void main(String args[]){
 		String names[] = {"ABC","XYZ","LMN"};
-		
+		//need to change
 		for(int i=0;i<names.length;i++){
 			System.out.println("Hi : "+names[i]);
 		}
