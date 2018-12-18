724d3c6592e49bf6b45e1c565faf4d4b1e009feb
shanti <abc@demo.in>
2018-12-18 12:42:27 +0530

    "Commit to see for multiple developers"

--- DemoForGit/src/DemoForGit.java
+++ DemoForGit/src/DemoForGit.java
@@ -5,6 +5,7 @@ public class DemoForGit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		//this is changed for demo purpose
 		System.out.println("This program is for git demo purpose: ");
 
 	}
---
+++ DemoOfGitForMultiDevelopers/src/Demo2.java
@@ -0,0 +1,13 @@
+
+public class Demo2 {
+	
+	public static void main(String args[]){
+		String names[] = {"ABC","XYZ","LMN"};
+		
+		for(int i=0;i<names.length;i++){
+			System.out.println("Hi : "+names[i]);
+		}
+		
+	}
+
+}
